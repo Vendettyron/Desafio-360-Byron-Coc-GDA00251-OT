d1a0f92361996079e39f28dda04d0b94a0f7aade
Vendettyron <cocpalomobyronalejandro@gmail.com>
2025-04-03 17:50:45 -0600

    ipv6 actualziaacion
    actualzaiacion para que el aplicativo funcione con ipv6

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ import logRoutes from './routes/log.routes.js';
 
 // Importar middleware de manejo de errores
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: true,
     credentials: true,
 };
 
@@ -53,8 +53,8 @@ app.use((req, res, next) => {
     res.status(404).json({ message: 'Ruta no encontrada o no importada desde server' });
 });
 
-// Iniciar el servidor
+// Iniciar el servidor, en IPv6 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto http://localhost:${PORT}`);
+app.listen(PORT, '::', () => {
+    console.log(`Servidor corriendo en puerto http://[::]:${PORT}`);
 });
